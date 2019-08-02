adfdb197998ee5a87b0d7f211089f0318a648826
STIG TOP GEAR <vmv9753@gmail.com>
2019-08-02 12:37:00 +0300

    v0.1

---
+++ index.js
@@ -0,0 +1,22 @@
+class Navigation extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "closed" });
+  }
+  connectedCallback() {
+    // fetch("chank.html")
+    //   .then(response => response.text())
+    //   .then(response => (this.shadow.innerHTML = response));
+  }
+  static get observedAttributes() {
+    return ["src"];
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (!newVal) return;
+    fetch(newVal)
+      .then(response => response.text())
+      .then(response => (this.shadow.innerHTML = response));
+  }
+}
+customElements.define("navigation-bar", Navigation);
+document.body.appendChild(document.createElement("navigation-bar"));
