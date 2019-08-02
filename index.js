8ae56a65ccadce3d03f71d58a73a0e15ce62ac5d
STIG TOP GEAR <vmv9753@gmail.com>
2019-08-02 13:55:31 +0300

    v0.3

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 class Navigation extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: "closed" });
+    this.shadow = this.attachShadow({ mode: "closed" })
   }
   connectedCallback() {
     // fetch("chank.html")
     //   .then(response => response.text())
-    //   .then(response => (this.shadow.innerHTML = response));
+    //   .then(response => (this.shadow.innerHTML = response))
   }
   static get observedAttributes() {
     return ["src"];
   }
   attributeChangedCallback(attrName, oldVal, newVal) {
-    if (!newVal) return;
+    if (!newVal) return
     fetch(newVal)
       .then(response => response.text())
-      .then(response => (this.shadow.innerHTML = response));
+      .then(response => (this.shadow.innerHTML = response))
   }
 }
-customElements.define("navigation-bar", Navigation);
-//document.body.appendChild(document.createElement("navigation-bar"));
+customElements.define("navigation-bar", Navigation)
+//document.body.appendChild(document.createElement("navigation-bar"))
 
 class Block_1st extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: "closed" });
+    this.shadow = this.attachShadow({ mode: "closed" })
   }
   connectedCallback() {
     // fetch("chank.html")
     //   .then(response => response.text())
-    //   .then(response => (this.shadow.innerHTML = response));
+    //   .then(response => (this.shadow.innerHTML = response))
   }
   static get observedAttributes() {
     return ["src"];
@@ -38,7 +38,29 @@ class Block_1st extends HTMLElement {
     if (!newVal) return;
     fetch(newVal)
       .then(response => response.text())
-      .then(response => (this.shadow.innerHTML = response));
+      .then(response => (this.shadow.innerHTML = response))
   }
 }
-customElements.define("block-1st", Block_1st);
+customElements.define("block-1st", Block_1st)
+
+class Block_media extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "closed" })
+  }
+  connectedCallback() {
+    // fetch("chank.html")
+    //   .then(response => response.text())
+    //   .then(response => (this.shadow.innerHTML = response))
+  }
+  static get observedAttributes() {
+    return ["src"];
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (!newVal) return;
+    fetch(newVal)
+      .then(response => response.text())
+      .then(response => (this.shadow.innerHTML = response))
+  }
+}
+customElements.define("block-media", Block_media)
