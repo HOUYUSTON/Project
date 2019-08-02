64b08e8264fba9fedd1a680e1c2e50e714977318
STIG TOP GEAR <vmv9753@gmail.com>
2019-08-02 14:01:30 +0300

    v0.3

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ class Block_1st extends HTMLElement {
     super();
     this.shadow = this.attachShadow({ mode: "closed" })
   }
-  connectedCallback() {
-    // fetch("chank.html")
-    //   .then(response => response.text())
-    //   .then(response => (this.shadow.innerHTML = response))
-  }
   static get observedAttributes() {
     return ["src"];
   }
@@ -48,11 +43,6 @@ class Block_media extends HTMLElement {
     super();
     this.shadow = this.attachShadow({ mode: "closed" })
   }
-  connectedCallback() {
-    // fetch("chank.html")
-    //   .then(response => response.text())
-    //   .then(response => (this.shadow.innerHTML = response))
-  }
   static get observedAttributes() {
     return ["src"];
   }
@@ -64,3 +54,20 @@ class Block_media extends HTMLElement {
   }
 }
 customElements.define("block-media", Block_media)
+
+class Block_2nd extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "closed" })
+  }
+  static get observedAttributes() {
+    return ["src"];
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (!newVal) return;
+    fetch(newVal)
+      .then(response => response.text())
+      .then(response => (this.shadow.innerHTML = response))
+  }
+}
+customElements.define("block-2nd", Block_2nd)
