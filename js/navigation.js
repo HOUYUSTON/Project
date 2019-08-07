5a26bf42b5e439c779c15cd8b782fd6414e811ad
STIG TOP GEAR <vmv9753@gmail.com>
2019-08-07 16:41:12 +0300

    v0.4

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,5 @@
 const root = document.getElementById("navigation").shadow
-const reg = root.getElementById("reg")
+const reg = root.getElementById("register")
 reg.style.color = "red"
 reg.onclick = function (event) {
 	document.getElementById("media") = document.createElement("block-map").src = "block-map.html"
