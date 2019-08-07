c9aa47a9747aa3cd6a0f0eae755abf8d0a72e74d
STIG TOP GEAR <vmv9753@gmail.com>
2019-08-07 15:55:18 +0300

    v0.4

--- navigation.js
+++
@@ -1,5 +0,0 @@
-const reg = document.getElementById("reg")
-reg.style.color = "red"
-reg.onclick = function (event) {
-	document.getElementById("media") = document.createElement("block-map").src = "block-map.html"
-}
